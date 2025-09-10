3a8e19c47b14200653a9c59c746dd8cfc7fd884c
gaolixin <gaolixin622@126.com>
2025-09-10 15:41:17 +0800

    config

---
+++ src/main/java/org/signal/registration/util/T.java
@@ -0,0 +1,12 @@
+package org.signal.registration.util;
+
+
+import io.micronaut.context.annotation.Configuration;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.context.annotation.EachProperty;
+import io.micronaut.scheduling.annotation.Scheduled;
+
+import io.micronaut.core.bind.annotation.Bindable;
+//    "${
