8491c2cb8b080695f8905dd504b2081b5f29b97e
gaolixin <gaolixin622@126.com>
2026-01-06 12:12:59 +0800

    aliyunsms-verify

--- src/main/java/org/signal/registration/sender/aliyun/AliyunSmsUtil.java
+++ src/main/java/org/signal/registration/sender/aliyun/AliyunSmsUtil.java
@@ -1,17 +1,6 @@
 package org.signal.registration.sender.aliyun;
 
 
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.dysmsapi.model.v20170525.QuerySmsTemplateRequest;
-import com.aliyuncs.dysmsapi.model.v20170525.QuerySmsTemplateResponse;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.ProtocolType;
-import com.aliyuncs.profile.DefaultProfile;
-import org.apache.commons.lang3.StringUtils;
-
 /**
  * @Author Jinyang
  * @Date 6/20/24
@@ -33,32 +22,20 @@ public class AliyunSmsUtil {
         String phoneNumber,
         String templateParam) {
 
-        DefaultProfile profile = DefaultProfile.getProfile(REGION_ID, accessKeyId, accessSecret);
-        profile.getHttpClientConfig().setProtocolType(ProtocolType.HTTPS);
-        IAcsClient client = new DefaultAcsClient(profile);
+      AliyunSmsOkHttpClient client = new AliyunSmsOkHttpClient(accessKeyId,
+          accessSecret);
+      String bizId = null;
+      try {
+        bizId = client.sendSms(
+            phoneNumber,
+            signName,
+            templateCode,
+            "{\""+templateParamKey+"\":\""+templateParam+"\"}"
+        );
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
 
-        // 创建API请求并设置参数
-        SendSmsRequest request = new SendSmsRequest();
-
-        // 该参数值为假设值，请您根据实际情况进行填写
-        request.setPhoneNumbers(phoneNumber);
-
-        // 该参数值为假设值，请您根据实际情况进行填写
-        request.setSignName(signName);
-
-        request.setTemplateCode(templateCode);
-
-        request.setTemplateParam("{\""+templateParamKey+"\":\""+templateParam+"\"}");
-        request.setEndpoint("dysmsapi.aliyuncs.com");
-        try {
-            SendSmsResponse response = client.getAcsResponse(request);
-            if (response.getCode() != null && "OK".equals(response.getCode())) {
-                return response.getBizId();
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            return null;
-        }
+      return bizId;
     }
 }
